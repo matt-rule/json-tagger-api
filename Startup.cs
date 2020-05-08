600936733026cea5dc80ffefcf8521a985635c4b
matt-rule <matt.rule.github@gmail.com>
2020-05-08 12:11:45 +0100

    Fix CORS issue

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace json_tagger_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddControllers();
         }
 
@@ -40,7 +41,10 @@ namespace json_tagger_api
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors(options => options
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
