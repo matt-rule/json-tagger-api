f2895da3fe3fdf8bdcff9ec2c9bda8c6dbdea59c
matt-rule <matt.rule.github@gmail.com>
2020-05-30 13:53:49 +0100

    Add code for search queries

--- Controllers/ImageListController.cs
+++ Controllers/ImageListController.cs
@@ -1,12 +1,15 @@
-using System.IO;
-using System.Linq;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 using JsonTaggerApi.Model;
 using JsonTaggerApi.Model.BusinessLogic;
 using JsonTaggerApi.Model.Json;
+using JsonTaggerApi.Model.EntityFrameworkModels;
 
 namespace JsonTaggerApi.Controllers
 {
@@ -27,12 +30,38 @@ namespace JsonTaggerApi.Controllers
             _dbContext = dbContext;
         }
 
-        [HttpGet]
-        public string Get()
+        [HttpGet("{query}/{page}")]
+        public string Get(string query, string page)
         {
+            IEnumerable<string> searchTagsUnsorted = query.Split(' ').Where(x => !String.IsNullOrWhiteSpace(x));
+            IEnumerable<string> searchTagsToInclude = searchTagsUnsorted.Where(x => !x.StartsWith('-'));
+            IEnumerable<string> searchTagsToExclude = searchTagsUnsorted.Where(x => x.StartsWith('-')).Select(x => x.Substring(1));
+
+            Func<IEnumerable<string>, IEnumerable<IQueryable<int>>> getFileIdsPerSearchSubStr = (IEnumerable<string> searchPortions) =>
+                searchPortions.Select(s => _dbContext.FileTagPairs.Where(pair => pair.Tag.Contains(s)).Select(x => x.FileId));
+
+            var fileIdsMatchingIncludeCriteria = getFileIdsPerSearchSubStr(searchTagsToInclude);
+            var fileIdsMatchingExcludeCriteria = getFileIdsPerSearchSubStr(searchTagsToExclude);
+
+            var includesQuery =
+                (searchTagsToInclude.Count() == 0)
+                ?
+                    _dbContext.FileRecords.AsQueryable()
+                :
+                    _dbContext
+                    .FileRecords
+                    .Where(
+                        fileRec => fileIdsMatchingIncludeCriteria.All(idSeq => idSeq.Contains(fileRec.Id))
+                    );
+
+            var excludesQuery =
+                includesQuery
+                .Where(
+                    fileRec => !fileIdsMatchingExcludeCriteria.Any(idSeq => idSeq.Contains(fileRec.Id))
+                );
+
             return
-                _dbContext
-                .FileRecords
+                excludesQuery
                 .Take(ITEMS_PER_PAGE)
                 .AsEnumerable()
                 .Select(x => (
